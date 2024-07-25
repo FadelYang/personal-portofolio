fc48584ce9635c030cc0889ee34542c7ecf9b28f
fadelanumah <fadelanumah@gmail.com>
2024-07-26 02:04:01 +0700

    delete unneccessary file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,78 +1,15 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    <!-- Styles -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-
-
-<body class="">
-    @include('components.guest-header')
-
-    <main>
-        {{-- hero section --}}
-        <div class="flex h-[calc(100vh-80px)]">
-            <div class="m-auto space-y-12 text-center">
-                <h1 class="text-4xl font-black text-black h1" id="headingHero"></h1>
-                <h2 class="max-w-2xl text-4xl font-black text-black h2 h1" id="secondaryHeadingHero">
-                </h2>
-                <button class="text-white btn btn-neutral">See My Works</button>
+<x-guest-layout>
+    <x-slot name="slot">
+        <main>
+            {{-- hero section --}}
+            <div class="flex h-[calc(100vh-80px)]">
+                <div class="m-auto space-y-12 text-center">
+                    <h1 class="text-4xl font-black text-black h1" id="headingHero"></h1>
+                    <h2 class="max-w-2xl text-4xl font-black text-black h2 h1" id="secondaryHeadingHero">
+                    </h2>
+                    <button class="text-white btn btn-neutral">See My Works</button>
+                </div>
             </div>
-        </div>
-    </main>
-</body>
-
-<script>
-    // Burger menus
-    document.addEventListener('DOMContentLoaded', function() {
-        // open
-        const burger = document.querySelectorAll('.navbar-burger');
-        const menu = document.querySelectorAll('.navbar-menu');
-
-        if (burger.length && menu.length) {
-            for (var i = 0; i < burger.length; i++) {
-                burger[i].addEventListener('click', function() {
-                    for (var j = 0; j < menu.length; j++) {
-                        menu[j].classList.toggle('hidden');
-                    }
-                });
-            }
-        }
-
-        // close
-        const close = document.querySelectorAll('.navbar-close');
-        const backdrop = document.querySelectorAll('.navbar-backdrop');
-
-        if (close.length) {
-            for (var i = 0; i < close.length; i++) {
-                close[i].addEventListener('click', function() {
-                    for (var j = 0; j < menu.length; j++) {
-                        menu[j].classList.toggle('hidden');
-                    }
-                });
-            }
-        }
-
-        if (backdrop.length) {
-            for (var i = 0; i < backdrop.length; i++) {
-                backdrop[i].addEventListener('click', function() {
-                    for (var j = 0; j < menu.length; j++) {
-                        menu[j].classList.toggle('hidden');
-                    }
-                });
-            }
-        }
-    });
-</script>
-
-</html>
+        </main>
+    </x-slot>
+</x-guest-layout>
