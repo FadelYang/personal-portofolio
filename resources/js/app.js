eeab5a3ea4ef2892a8f877fa4f06ab42fc6c8c70
fadelanumah <fadelanumah@gmail.com>
2024-07-25 20:14:19 +0700

    feat(menu-hero): Add Typed Js for Type Animating Load

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
-import './bootstrap';
+import "./bootstrap";
+
+import Typed from "typed.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed("#headingHero", {
+        strings: ["👋 Hello, I’m Fadel"],
+        typeSpeed: 50,
+        showCursor: false,
+        smartBackspace: true,
+        loop: false,
+        onComplete: function () {
+            // Initialize the second Typed instance after the first one completes
+            new Typed("#secondaryHeadingHero", {
+                strings: [
+                    "Fullstack web developer 🌐💻 based in Jakarta, Indonesia",
+                ],
+                typeSpeed: 50,
+                showCursor: false,
+                smartBackspace: true,
+                loop: false,
+            });
+        },
+    });
+});
